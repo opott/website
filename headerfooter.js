a3b64b14eda6126c0f794d8f7c5dab9ea2469406
Oliver P. <GitHub@opott.is-cool.me>
2024-07-06 18:35:18 +0000

    feat: Add navigation links to GitHub and Instagram in footer

--- headerfooter.js
+++ headerfooter.js
@@ -22,6 +22,12 @@ class DynFooter extends HTMLElement {
     this.innerHTML = `
         <link rel="stylesheet" href="/styles.css">
         <footer>
+          <nav>
+              <ul>
+                  <li><a href="https://github.com/opott" target="_blank">GitHub</a></li>
+                  <li><a href="https://www.instagram.com/ompot09/" target="_blank">Instagram</a></li>
+              </ul>
+          </nav>
             <p>&copy; 2024 Oliver Potter. All rights reserved.</p>
         </footer>
         `;
